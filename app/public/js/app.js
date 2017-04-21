269e8dd5585eb025f6970ece627260d0c7ef3d93
Erin <eringlabe@gmail.com>
2017-04-20 20:28:52 -0700

    jquery to capture user input

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -5,8 +5,28 @@ var currentURL = window.location.origin;
 // POST data to my endpoint 
 // $.POST(endpoint-url)
 
-$('button').on('click', function(e){
+$("#submitBtn").on('click', function(e){
     //e.preventDefault();
+    var newUser = {
+        name: $("#userName").val().trim(),
+        photo: $("#userPhoto").val().trim(),
+        scores: {
+            $("#q1").val(),
+            $("#q2").val(),
+            $("#q3").val(),
+            $("#q4").val(),
+            $("#q5").val(),
+            $("#q6").val(),
+            $("#q7").val(),
+            $("#q8").val(),
+            $("#q9").val(),
+            $("#q10").val()
+        }
+    };
+
+
+
+
     $.post( "/api/friends", { name: "John", time: "2pm" }, function( data ) {
         console.log(data);
     });
